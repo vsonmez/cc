883476751372849e29e1440167106b18dc62cdc0
vsonmez <hvsonmez@gmail.com>
2025-10-29 12:15:10 +0300

    clean branch

--- src/core/storage/index.ts
+++
@@ -1,7 +0,0 @@
-// Barrel export for storage layer
-// Why: Allows importing all repository functions from a single entry point
-
-export * from './childRepository';
-export * from './taskRepository';
-export * from './settingsRepository';
-export * from './storage-engine';
